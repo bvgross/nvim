6b0c5f1037ae143ab0e3278df98cc193bc2c1649
Bruno Ventura Gross <bvgross@gmx.com>
2025-04-09 01:02:43 -0300

    New nvim config, removed many unused files and corrected some other

--- lua/community.lua
+++ lua/community.lua
@@ -8,8 +8,5 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.html-css" },
   -- import/override with your plugins folder
 }
